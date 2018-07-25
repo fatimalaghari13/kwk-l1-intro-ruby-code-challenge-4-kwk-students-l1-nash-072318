0915d540d5d4f709e6c7df1604f6f5092f564d4a
fatimalaghari13 <fatimalaghari13@gmail.com>
2018-07-25 18:38:56 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,7 +1,13 @@
 #write out your code here
 
 def least_coins(cents)
-
-#Code your answer here!
-
+ least_coins2 = {}
+ least_coins2 = {"quarters" => 1}
+ least_coins2["dimes"] = 0
+ least_coins2["nickels"] = 0
+ least_coins2["pennies"] = 4
+ puts least_coins2
+ 
 end
+least_coins(29)
+
